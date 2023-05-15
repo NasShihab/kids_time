b5e6cab16b3d188419a41bce52c00762909f2a2a
Nas <nas.shihab92@gmail.com>
2023-05-15 21:22:34 +0600

    const

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
