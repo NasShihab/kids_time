09ad0625db2d8fabd5fa42593257be49615b3119
Nas <nas.shihab92@gmail.com>
2023-05-23 21:32:08 +0600

    reading page 1.1

--- lib/dashboard/category/reading/reading_page.dart
+++ lib/dashboard/category/reading/reading_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kids_time/dashboard/category/reading/reading_data.dart';
 
 class ReadingPage extends StatelessWidget {
@@ -7,24 +8,42 @@ class ReadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Reading Section'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('images/background02.jpg'),
+        ),
       ),
-      body: SafeArea(
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 15.w,
-              mainAxisSpacing: 15.h,
-              crossAxisCount: 3,
-              childAspectRatio: 4 / 3),
-          itemCount: dayNames.length,
-          itemBuilder: (context, index) {
-            return myContainer(
-              text: dayNames[index],
-            );
-          },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Reading Section'),
+        ),
+        body: SafeArea(
+          child: ListView(
+            children: [
+              myButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const Months(),
+                    ),
+                  );
+                }, title: 'Months Names',
+              ),
+              myButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const Months(),
+                    ),
+                  );
+                }, title: 'Day\'s Names',
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -32,15 +51,13 @@ class ReadingPage extends StatelessWidget {
 }
 
 Widget myContainer({required String text}) => Container(
-      width: 200.w,
-      height: 50.h,
       decoration: BoxDecoration(
         gradient: const LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: [
             Colors.red,
-            Colors.green,
+            Colors.yellow,
           ],
         ),
         borderRadius: BorderRadius.circular(10),
@@ -53,13 +70,94 @@ Widget myContainer({required String text}) => Container(
         ],
       ),
       child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.yellow,
-            fontSize: 25.sp,
-            fontWeight: FontWeight.bold,
+        child: FittedBox(
+          child: Text(
+            text,
+            style: GoogleFonts.secularOne(
+              color: Colors.black,
+              fontSize: 32.sp,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
     );
+
+Widget myButton({required VoidCallback onPressed,required String title}) => ElevatedButton(
+  onPressed: onPressed,
+  style: ElevatedButton.styleFrom(
+    foregroundColor: Colors.white,
+    backgroundColor: Colors.transparent,
+    elevation: 5,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.all(15),
+    textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    shadowColor: Colors.transparent,
+  ),
+  child: Ink(
+    decoration: BoxDecoration(
+      gradient: const LinearGradient(
+        colors: [
+          Colors.red,
+          Colors.green,
+        ],
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Container(
+      constraints:
+      BoxConstraints(maxWidth: double.infinity.w, minHeight: 100.h),
+      alignment: Alignment.center,
+      child: Text(
+        title,
+        style: GoogleFonts.secularOne(color: Colors.white, fontSize: 40.sp),
+      ),
+    ),
+  ),
+);
+
+
+class Months extends StatelessWidget {
+  const Months({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('images/background02.jpg'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Reading Section'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.w),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisSpacing: 15.w,
+                  mainAxisSpacing: 15.h,
+                  crossAxisCount: 2,
+                  childAspectRatio: 6 / 3),
+              itemCount: dayNames.length,
+              itemBuilder: (context, index) {
+                return myContainer(
+                  text: dayNames[index],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
